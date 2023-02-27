1e2709838e5fdec3fd881d6a1f987bb737e81cc4
Maxime Vlaminck <maxime.vlaminck@student.vinci.be>
2023-02-27 12:42:05 +0100

    fin seance 27/02

--- seance1/src/ex4/ChoixOptions.java
+++ seance1/src/ex4/ChoixOptions.java
@@ -1,13 +1,14 @@
 package ex4;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class ChoixOptions {
 	
 	// associe le nom d'une option avec son objet Option correspondant
 	private Map<String, Option> options;
 	// ajouter ici les autres attributs
+	private Map<Etudiant, ArrayList<Option>> choixEtudiants;
+	private TreeSet<Etudiant> etudiantsTriesParMoyenne;
 	
 	
 	//constructeur prenant un entier et une suite de string en paramètres
@@ -21,12 +22,19 @@ public class ChoixOptions {
 			options.put(nomOption, new Option(nomOption, nbEtudiantsParOption));
 		}
 		// initialiser les nouveaux attributs
+		choixEtudiants = new HashMap<>();
+		etudiantsTriesParMoyenne = new TreeSet<>(Comparator.comparing(Etudiant::getMoyenne).reversed());
 	}
 
 	// cette méthode encode les préférences des étudiants
 	// il ne faut pas vérifier que ces choix soient valides
-	public void ajouterPreferences(Etudiant etu, String choix1, String choix2,
-			String choix3) {
+	public void ajouterPreferences(Etudiant etu, String choix1, String choix2, String choix3) {
+		etudiantsTriesParMoyenne.add(etu);
+		ArrayList<Option> list = new ArrayList<>();
+		list.add(options.get(choix1));
+		list.add(options.get(choix2));
+		list.add(options.get(choix3));
+		choixEtudiants.put(etu, list);
 	}
 
 	// cette méthode est appelée après que les étudiants aient donné leurs préférences
@@ -36,6 +44,12 @@ public class ChoixOptions {
 	// il faut recourir au troisième choix.
 	// Cette méthode doit faire appel à la méthode inscrireEtudiant de la classe Option.
 	public void attribuerOptions() {
+		for (Etudiant etu : etudiantsTriesParMoyenne){
+			ArrayList<Option> choixDeLEtu = choixEtudiants.get(etu);
+			for (int i = 0 ; i < choixDeLEtu.size();i++){
+				if (choixDeLEtu.get(i).inscrireEtudiant(etu)) break;
+			}
+		}
 	}
 	
 	public String toString(){
--- seance1/src/ex5/Doodle.java
+++ seance1/src/ex5/Doodle.java
@@ -1,23 +1,35 @@
 package ex5;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+
 public class Doodle {
 	private PlageHoraire[] plages;
 	// a compléter
+	private HashMap<String,boolean[]> dispoParticipants;
 
 	public Doodle(PlageHoraire... plages) {
 		this.plages = plages;
 		// a compléter
+		dispoParticipants = new HashMap<>();
 	}
 
 	// ajoute les disponibilités d'un participant sous forme d'un tableau de booleen.
 	// les indices du tableau correspondent aux indices du tableau plages
 	// true à l'indice i veut dire que le participant est disponible pour la plage à l'indice i du tableau plages
 	// false à l'indice i veut dire que le participant n'est pas disponible pour la plage à l'indice i du tableau plages
-	public void ajouterDisponibilites(String participant,
-			boolean[] disponibilites) {
+	public void ajouterDisponibilites(String participant, boolean[] disponibilites) {
 		if (disponibilites.length != plages.length)
 			throw new IllegalArgumentException();
 		//a compléter
+		dispoParticipants.put(participant,disponibilites);
+;
+		for (int i = 0 ;  i<plages.length; i++){
+			if (disponibilites[i]){
+				plages[i].setNbParticipantPresent(plages[i].getNbParticipantPresent()+1);
+			}
+		}
 	}
 	
 	// Hypothèse: la PlageHoraire plage en paramètre fait bien partie du tableau plages
@@ -25,13 +37,21 @@ public class Doodle {
 	// renvoie faux si le participant n'est pas disponible ou s'il n'a pas rempli le
 	// sondage doodle
 	public boolean estDisponible(String participant, PlageHoraire plage) {
-		return false;
+		boolean[] dispoDuParticipant = dispoParticipants.get(participant);
+		int indexPlage = 0;
+		for (int i = 0 ; i< plages.length;i++){
+			if (plages[i].equals(plage)) {
+				indexPlage = i;
+				break;
+			}
+		}
+		return dispoDuParticipant[indexPlage];
 	}
 
 	// renvoie une des plages horaires qui a le maximum de participants prévus
 	// cette méthode est appelée après que les participants aient rempli leurs disponibilités
 	public PlageHoraire trouverPlageQuiConvientLeMieux() {
-		return null;
+		return Arrays.stream(plages).sorted(Comparator.comparing(PlageHoraire::getNbParticipantPresent).reversed()).findFirst().orElse(null);
 	}
 
 }
