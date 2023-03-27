7c6891c6df86404c7fde13f34335031222057d5a
Maxime Vlaminck <maxime.vlaminck@student.vinci.be>
2023-03-27 15:44:45 +0200

    badaboum boum boum

--- seance3/src/Main.java
+++ seance3/src/Main.java
@@ -4,11 +4,14 @@ public class Main {
     Tree l3 = new Tree(3);
     Tree l5 = new Tree(5);
     Tree l7 = new Tree(7);
-    
+    Tree l3v2 = new Tree(3);
+
     Tree t2 = new Tree(2, new Tree[]{l1, l3});
+    Tree t2v2 = new Tree(2, new Tree[]{l1, l3v2});
     Tree t6 = new Tree(6, new Tree[]{l7});
     
     Tree t4 = new Tree(4, new Tree[]{t2, l5, t6});
+    Tree t1 = new Tree(1, new Tree[]{t2, l5, t6,t4});
 
     System.out.println(Trees.nbrLeaves(t4));
     
@@ -26,5 +29,22 @@ public class Main {
 
     System.out.println("Path V2");    
     Trees.pathV2(l7);
+
+    System.out.println("Nbr nodes =>");
+    System.out.println(Trees.nbrNode(t2));
+    System.out.println(Trees.nbrNode(t4));
+
+    System.out.println("Min =>");
+    System.out.println(Trees.min(t4));
+    System.out.println(Trees.min(t1));
+
+    System.out.println("Seum =>");
+    System.out.println(Trees.sum(t4));
+    System.out.println(Trees.sum(t1));
+
+    System.out.println("Equals =>");
+    System.out.println("False => " + Trees.equals(t4,t1));
+    System.out.println("True =>" + Trees.equals(t2,t2v2));
+
   }
 }
--- seance3/src/Trees.java
+++ seance3/src/Trees.java
@@ -1,140 +1,161 @@
 public class Trees {
 
-	// *******************************************************
-	// Un premier exemple: le nombre de feuilles d'un arbre
-	// *******************************************************
-	public static int nbrLeaves(Tree t) {
-		int r;
-		if (t.isLeaf()) {
-			r = 1;
-		} else {
-			r = 0;
-			for (Tree child : t) {
-				r += nbrLeaves(child);
-			}
-		}
-		return r;
-	}
+    // *******************************************************
+    // Un premier exemple: le nombre de feuilles d'un arbre
+    // *******************************************************
+    public static int nbrLeaves(Tree t) {
+        int r;
+        if (t.isLeaf()) {
+            r = 1;
+        } else {
+            r = 0;
+            for (Tree child : t) {
+                r += nbrLeaves(child);
+            }
+        }
+        return r;
+    }
 
-	// *******************************************************
-	// Un deuxième exemple: aplanir un arbre
-	// *******************************************************
-	public static Tree[] flattenLeaves(Tree t) {
-		int nl = nbrLeaves(t);
-		Tree[] r = new Tree[nl];
-		flattenLeaves(t, r, 0);
-		return r;
-	}
+    // *******************************************************
+    // Un deuxième exemple: aplanir un arbre
+    // *******************************************************
+    public static Tree[] flattenLeaves(Tree t) {
+        int nl = nbrLeaves(t);
+        Tree[] r = new Tree[nl];
+        flattenLeaves(t, r, 0);
+        return r;
+    }
 
-	private static int flattenLeaves(Tree t, Tree[] a, int idx) {
-		int r;
-		if (t.isLeaf()) {
-			a[idx] = t;
-			r = 1;
-		} else {
-			r = 0;
-			for (Tree child : t) {
-				r += flattenLeaves(child, a, idx + r);
-			}
-		}
-		return r;
-	}
+    private static int flattenLeaves(Tree t, Tree[] a, int idx) {
+        int r;
+        if (t.isLeaf()) {
+            a[idx] = t;
+            r = 1;
+        } else {
+            r = 0;
+            for (Tree child : t) {
+                r += flattenLeaves(child, a, idx + r);
+            }
+        }
+        return r;
+    }
 
-	// *******************************************************
-	// Un troisième exemple:
-	// tous les algorithmes ne sont pas récursifs
-	// *******************************************************
-	public static void pathV1(Tree t) {
-		System.out.println(t.getValue());
-		if (t.getParent() != null) {
-			pathV1(t.getParent());
-		}
-	}
+    // *******************************************************
+    // Un troisième exemple:
+    // tous les algorithmes ne sont pas récursifs
+    // *******************************************************
+    public static void pathV1(Tree t) {
+        System.out.println(t.getValue());
+        if (t.getParent() != null) {
+            pathV1(t.getParent());
+        }
+    }
 
-	public static void pathV2(Tree t) {
-		while (t != null) {
-			System.out.println(t.getValue());
-			t = t.getParent();
-		}
-	}
+    public static void pathV2(Tree t) {
+        while (t != null) {
+            System.out.println(t.getValue());
+            t = t.getParent();
+        }
+    }
 
-	// *******************************************************
-	// Exercices 1
-	// *******************************************************
+    // *******************************************************
+    // Exercices 1
+    // *******************************************************
 
-	// 1.1)
-	public static int nbrNode(Tree t) {
-		return 42;
-	}
+    // 1.1)
+    public static int nbrNode(Tree t) {
+        int cmpt = 1;
+        if (t.isLeaf())
+            return 1;
+        for (Tree child : t) {
+            cmpt += nbrNode(child);
+        }
+        return cmpt;
+    }
 
-	// 1.2)
-	public static int min(Tree t) {
-		return 42;
-	}
+    // 1.2)
+    public static int min(Tree t) {
+        int min = t.getValue();
 
-	// 1.3)
-	public static int sum(Tree t) {
-		return 42;
-	}
+        for (Tree child : t) {
+            int minChild = min(child);
+            if (minChild < min) min = minChild;
+        }
+        return min;
+    }
 
-	// 1.4)
-	public static boolean equals(Tree t1, Tree t2) {
-		return false;
-	}
+    // 1.3)
+    public static int sum(Tree t) {
+        int sum = t.getValue();
 
-	// 1.5)
-	public static int depth(Tree n) {
-		return 42;
-	}
+        for (Tree child : t) {
+            sum += sum(child);
+        }
+        return sum;
+    }
 
-	// 1.6)
-	public static boolean sameOne(Tree n1, Tree n2) {
-		return true;
-	}
+    // 1.4)
+    public static boolean equals(Tree t1, Tree t2) {
+        if (t1.getValue() != t2.getValue() || t1.nbrChildren() != t2.nbrChildren()) return false;
+        for (int i = 0; i < t1.nbrChildren(); i++) {
+            if (!equals(t1.getChildren()[i], t2.getChildren()[i])) return false;
+        }
+        return true;
+    }
 
-	// 1.7)
-	public static void dfsPrint(Tree t) {
-	}
+    // 1.5)
+    public static int depth(Tree n) {
+        return 42;
+    }
 
-	// 1.8)
-	public static void bfsPrint(Tree t) {
-	}
+    // 1.6)
+    public static boolean sameOne(Tree n1, Tree n2) {
+        return true;
+    }
 
-	// *******************************************************
-	// Exercices 2
-	// *******************************************************
+    // 1.7)
+    public static void dfsPrint(Tree t) {
+    }
 
-	// 2.1)
-	static void printPathV1(Tree node) {
-	}
+    // 1.8)
+    public static void bfsPrint(Tree t) {
+    }
 
-	// 2.2)
-	static void printPathV2(Tree node) {
-	}
+    // *******************************************************
+    // Exercices 2
+    // *******************************************************
 
-	// 2.3
-	static void printPathV3(Tree t, int v) {
-	}
+    // 2.1)
+    static void printPathV1(Tree node) {
+    }
 
-	// *******************************************************
-	// Exercices 3
-	// *******************************************************
+    // 2.2)
+    static void printPathV2(Tree node) {
+    }
 
-	// 3.1
-	public static int[][] toArray(Tree t) {
-		return null;
-	}
+    // 2.3
+    static void printPathV3(Tree t, int v) {
+    }
 
-	// 3.2
-	public static Tree toTree(int[][] t) {
-		return null;
-	}
+    // *******************************************************
+    // Exercices 3
+    // *******************************************************
 
-	// *******************************************************
-	// Exercices 4
-	// *******************************************************
+    // 3.1
+    public static int[][] toArray(Tree t) {
+        return null;
+    }
 
-	public static Tree lca(Tree n1, Tree n2) {
-		return null;
-	}
+    // 3.2
+    public static Tree toTree(int[][] t) {
+        return null;
+    }
+
+    // *******************************************************
+    // Exercices 4
+    // *******************************************************
+
+    public static Tree lca(Tree n1, Tree n2) {
+        return null;
+    }
 }
